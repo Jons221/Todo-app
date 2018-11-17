9b7ea51d6577c4e29cdd0bef0c53bb64c9911740
John Doe <johndoe@example.com>
2018-11-17 02:37:16 -0500

    todo app

---
+++ index.js
@@ -0,0 +1,16 @@
+function morning(){
+    document.getElementById("morning").innerHTML="Good morning" 
+}
+function afternoon()
+{
+    document.getElementById("morning").innerHTML="Good afternoon"
+}
+function evening()
+{
+    document.getElementById("morning").innerHTML="Good evening"
+}
+function Jhon()
+{
+    document.getElementById("nama").innerHTML="Jhon doe"
+    
+}
